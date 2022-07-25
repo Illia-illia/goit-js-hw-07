a3eeb727d643e4093a9637acf3e8c06054f29e0d
Illia-Illia <Takoexerrorx@gmail.com>
2022-07-25 10:38:13 +0300

    create markup

---
+++ js/01-gallery.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryRenderMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryRenderMarkup);
+
+galleryEl.addEventListener('click', getLargeImg);
+
+function createGalleryMarkup(data) {
+  return data
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join('');
+}
+
+function getLargeImg(e) {
+  e.preventDefault();
+  console.log(e.target);
+}
