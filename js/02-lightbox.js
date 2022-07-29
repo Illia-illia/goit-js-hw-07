60e64a8d193c41b19b05118478d83c6c996d45ef
Illia-Illia <Takoexerrorx@gmail.com>
2022-07-29 12:35:47 +0300

    finish task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryListEl = document.querySelector('.gallery');
-galleryListEl.addEventListener('click', modalWindow);
 
-function createMarkUp(array) {
-  return array
+function createMarkUp(obj) {
+  return obj
     .map(
       ({ preview, original, description }) => `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
@@ -14,11 +13,7 @@ function createMarkUp(array) {
 }
 galleryListEl.innerHTML = createMarkUp(galleryItems);
 
-function modalWindow(e) {
-  e.preventDefault();
-}
 const lightbox = new SimpleLightbox('.gallery a', {
   /* options */
 });
-
-console.log(lightbox.options);
+lightbox.options.captionDelay = '250';
